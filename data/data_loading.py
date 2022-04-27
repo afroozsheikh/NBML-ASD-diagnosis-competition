51b5ea4f8ae8999a11b00e08a6abbf529b0a167b
Afrooz Sheikholeslami <52376098+afroozsheikh@users.noreply.github.com>
2022-04-27 13:22:24 +0430

    Feat data (#1)
    * parametrizing connectivity kind: tangent, ...
    
    * adding train, val data loaders

--- data/data_loading.py
+++ data/data_loading.py
@@ -24,64 +24,88 @@ def parse_arguments():
         help="Path to the output file",
         required=True,
     )
+    parser.add_argument(
+        "--fc_matrix_kind",
+        type=str,
+        default="correlation",
+        help="different kinds of functional connectivity matrices : covariance, correlation, partial correlation, tangent, precision",
+        required=False,
+    )
     args = parser.parse_args()
     return args
 
 
-def load_data(data_dir, output_dir):
+def load_data(data_dir, output_dir, fc_matrix_kind):
 
     # initialize correlation measure
     correlation_measure = ConnectivityMeasure(
-        kind="correlation", vectorize=False, discard_diagonal=True
+        kind=fc_matrix_kind, vectorize=False, discard_diagonal=True
     )
 
     try:  # check if feature file already exists
         # load features
-        feat_file = os.path.join(output_dir, "ABIDE_adjacency.npz")
-        adj_mat = np.load(feat_file)["a"]
+        feat_file = os.path.join(
+            output_dir, "ABIDE_adjacency_" + fc_matrix_kind + ".npz"
+        )
+        correlation_matrices = np.load(feat_file)["a"]
 
         y_target = os.path.join(output_dir, "Y_target.npz")
         y_target = np.load(y_target)["a"]
         print("Feature file found.")
 
     except:  # if not, extract features
-        adj_mat = []
-        y_target = []
         print("No feature file found. Extracting features...")
+        time_series_ls = []
+        correlation_matrices = []
+        y_target = []
 
         for (root, dirs, files) in tqdm(os.walk(data_dir, topdown=True), position=0):
             for file in files:
                 if file.endswith(".csv"):
                     path = os.path.join(root, file)
-                    time_series = pd.read_csv(path).to_numpy()
-                    # print(f"shape of time series : {time_series.shape}") # (176, 111)
+
+                    ## creating y
                     if "ASD" in root:
                         y_target.append(1)
                     else:
                         y_target.append(0)
-                    correlation_matrix = correlation_measure.fit_transform(
-                        [time_series]
-                    )[0]
 
-                    adj_mat.append(correlation_matrix)
+                    time_series = pd.read_csv(path).to_numpy()
+                    print(f"shape of time series : {time_series.shape}")  # (176, 111)
 
-        np.savez_compressed(os.path.join(output_dir, "ABIDE_adjacency"), a=adj_mat)
-        adj_mat = np.array(adj_mat)
+                    if fc_matrix_kind == "tangent":
+                        time_series_ls.append(time_series)
+
+                    else:
+                        correlation_matrix = correlation_measure.fit_transform(
+                            [time_series]
+                        )[0]
+                        correlation_matrices.append(correlation_matrix)
+
+        if fc_matrix_kind == "tangent":
+            correlation_matrices = correlation_measure.fit_transform(time_series_ls)
+
+        np.savez_compressed(
+            os.path.join(output_dir, "ABIDE_adjacency_" + fc_matrix_kind),
+            a=correlation_matrices,
+        )
+        correlation_matrices = np.array(correlation_matrices)
 
         np.savez_compressed(os.path.join(output_dir, "Y_target"), a=y_target)
         y_target = np.array(y_target)
 
-    return adj_mat, y_target
+    return correlation_matrices, y_target
 
 
 def run():
     args = parse_arguments()
-    adj_mat, y_target = load_data(args.train_data_path, args.output_path)
+    adj_mat, y_target = load_data(
+        args.train_data_path, args.output_path, args.fc_matrix_kind
+    )
     print(adj_mat.shape)
     print("***************************")
     print(y_target.shape)
     print("***************************")
-    print(adj_mat[0])
 
 
 if __name__ == "__main__":
--- data/data_preparation.py
+++ data/data_preparation.py
@@ -13,6 +13,7 @@ import torch
 from torch_geometric.data import Data
 from torch_geometric.loader import DataLoader
 from torch_geometric.data import InMemoryDataset
+from sklearn.model_selection import train_test_split
 
 
 def parse_arguments():
@@ -22,14 +23,14 @@ def parse_arguments():
     parser.add_argument(
         "--adj_path",
         type=str,
-        default=r"C:\Users\Afrooz Sheikholeslam\Education\8th semester\Project1\Code\Out\ABIDE_adjacency.npz",
+        default=r"C:\Users\Afrooz Sheikholeslam\Education\8th semester\Project1\competition\out\ABIDE_adjacency.npz",
         help="Path to the adjacancy matrix",
         required=True,
     )
     parser.add_argument(
         "--y_path",
         type=str,
-        default=r"C:\Users\Afrooz Sheikholeslam\Education\8th semester\Project1\Code\Out\Y_target.npz",
+        default=r"C:\Users\Afrooz Sheikholeslam\Education\8th semester\Project1\competition\out\Y_target.npz",
         help="Path to the y target",
         required=True,
     )
@@ -40,12 +41,19 @@ def parse_arguments():
         help="Size of batch",
         required=False,
     )
+    parser.add_argument(
+        "--threshold",
+        type=float,
+        default=0.5,
+        help="Threshold",
+        required=False,
+    )
 
     args = parser.parse_args()
     return args
 
 
-def data_preparation(adj_path, y_path, batch_size=1):
+def data_preparation(adj_path, y_path, batch_size=1, threshold=0.6):
     """Creates Data object of pytorch_geometric using graph features and edge list
 
     Parameters
@@ -63,8 +71,9 @@ def data_preparation(adj_path, y_path, batch_size=1):
 
     label = np.load(y_path)
     y_target = label["a"]
+    print(y_target.shape)
 
-    adj_mat = np.greater_equal(adj_mat, 0.5).astype(int)
+    adj_mat = np.greater_equal(adj_mat, threshold).astype(int)
 
     data_list = []
     ## Create a graph using networkx
@@ -75,7 +84,9 @@ def data_preparation(adj_path, y_path, batch_size=1):
         features = pd.DataFrame(
             {
                 "degree": dict(G.degree).values(),
-                "eigen_vector_centrality": dict(nx.eigenvector_centrality(G)).values(),
+                "eigen_vector_centrality": dict(
+                    nx.eigenvector_centrality(G, tol=1.0e-3)
+                ).values(),
                 "betweenness": dict(betweenness_centrality(G)).values(),
                 "closeness": dict(closeness_centrality(G)).values(),
                 "clustring_coef": dict(clustering(G)).values(),
@@ -88,17 +99,25 @@ def data_preparation(adj_path, y_path, batch_size=1):
         # print(y_target[i].item())
         data_list.append(Data(x=X, edge_index=edge_index.T, y=y_target[i].item()))
 
-    loader = DataLoader(data_list, batch_size=batch_size)
-    # data, slices = InMemoryDataset.collate(data_list) --> Not needed : collects and comibes all graphs to one graph
-    return loader
+    ## Split Dataset
+    train, val = train_test_split(
+        data_list, test_size=0.2, stratify=y_target, random_state=42
+    )
+
+    train_data_loader = DataLoader(train, batch_size=batch_size)
+    val_data_loader = DataLoader(val, batch_size=batch_size)
+
+    return train_data_loader, val_data_loader
 
 
 def main():
     args = parse_arguments()
-    loader = data_preparation(args.adj_path, args.y_path, args.batch_size)
-    for data in loader:
-        print(data)
-        print(data.y)
+    train_data_loader, val_data_loader = data_preparation(
+        args.adj_path, args.y_path, args.batch_size, args.threshold
+    )
+
+    for i, data in enumerate(train_data_loader):  # every batch
+        print(i, data, data.y)
 
 
 if __name__ == "__main__":
