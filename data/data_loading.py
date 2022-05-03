a6673054c7457795584ff1643b650bb5e3496e62
Afrooz Sheikholeslami <52376098+afroozsheikh@users.noreply.github.com>
2022-05-03 22:41:05 +0430

    Feat data (#3)
    * test data added
    
    * time series saved
    
    * time series features added
    
    * test features added

--- data/data_loading.py
+++ data/data_loading.py
@@ -18,6 +18,13 @@ def parse_arguments():
         required=True,
     )
     parser.add_argument(
+        "--test_data_path",
+        type=str,
+        default=r"C:\Users\Afrooz Sheikholeslam\Education\8th semester\Project1\competition\Primary Test Data",
+        help="Path to the test data folder containing .csv files",
+        required=True,
+    )
+    parser.add_argument(
         "--output_path",
         type=str,
         default=r"C:\Users\Afrooz Sheikholeslam\Education\8th semester\Project1\competition\out",
@@ -35,7 +42,7 @@ def parse_arguments():
     return args
 
 
-def load_data(data_dir, output_dir, fc_matrix_kind):
+def load_data(data_dir, test_data_dir, output_dir, fc_matrix_kind):
 
     # initialize correlation measure
     correlation_measure = ConnectivityMeasure(
@@ -45,18 +52,32 @@ def load_data(data_dir, output_dir, fc_matrix_kind):
     try:  # check if feature file already exists
         # load features
         feat_file = os.path.join(
-            output_dir, "ABIDE_adjacency_" + fc_matrix_kind + ".npz"
+            output_dir, "train_adjacency_" + fc_matrix_kind + ".npz"
         )
         correlation_matrices = np.load(feat_file)["a"]
 
+        test_feat_file = os.path.join(
+            output_dir, "test_adjacency_" + fc_matrix_kind + ".npz"
+        )
+        test_correlation_matrices = np.load(test_feat_file)["a"]
+
         y_target = os.path.join(output_dir, "Y_target.npz")
         y_target = np.load(y_target)["a"]
+
+        time_series_ls = os.path.join(output_dir, "time_series.npz")
+        time_series_ls = np.load(time_series_ls)["a"]
+
+        test_time_series_ls = os.path.join(output_dir, "test_time_series.npz")
+        test_time_series_ls = np.load(test_time_series_ls)["a"]
+
         print("Feature file found.")
 
     except:  # if not, extract features
         print("No feature file found. Extracting features...")
         time_series_ls = []
+        test_time_series_ls = []
         correlation_matrices = []
+        test_correlation_matrices = []
         y_target = []
 
         for (root, dirs, files) in tqdm(os.walk(data_dir, topdown=True), position=0):
@@ -70,13 +91,15 @@ def load_data(data_dir, output_dir, fc_matrix_kind):
                     else:
                         y_target.append(0)
 
-                    time_series = pd.read_csv(path).to_numpy()
-                    print(f"shape of time series : {time_series.shape}")  # (176, 111)
+                    time_series = pd.read_csv(path)
+                    time_series.drop(time_series.columns[0], axis=1, inplace=True)
+                    time_series = time_series.to_numpy()
+                    print(f"shape of time series : {time_series.shape}")  # (176, 110 )
 
-                    if fc_matrix_kind == "tangent":
-                        time_series_ls.append(time_series)
+                    # if fc_matrix_kind == "tangent":
+                    time_series_ls.append(time_series)
 
-                    else:
+                    if fc_matrix_kind != "tangent":
                         correlation_matrix = correlation_measure.fit_transform(
                             [time_series]
                         )[0]
@@ -85,26 +108,89 @@ def load_data(data_dir, output_dir, fc_matrix_kind):
         if fc_matrix_kind == "tangent":
             correlation_matrices = correlation_measure.fit_transform(time_series_ls)
 
+        for (root, dirs, files) in tqdm(
+            os.walk(test_data_dir, topdown=True), position=0
+        ):
+            for file in files:
+                if file.endswith(".csv"):
+                    path = os.path.join(root, file)
+
+                    time_series = pd.read_csv(path)
+
+                    test_time_series = pd.read_csv(path)
+                    test_time_series.drop(
+                        test_time_series.columns[0], axis=1, inplace=True
+                    )
+                    test_time_series = test_time_series.to_numpy()
+                    print(
+                        f"shape of time series : {test_time_series.shape}"
+                    )  # (176, 110)
+
+                    # if fc_matrix_kind == "tangent":
+                    test_time_series_ls.append(test_time_series)
+
+                    if fc_matrix_kind != "tangent":
+                        test_correlation_matrix = correlation_measure.fit_transform(
+                            [test_time_series]
+                        )[0]
+                        test_correlation_matrices.append(test_correlation_matrix)
+
+        if fc_matrix_kind == "tangent":
+            test_correlation_matrices = correlation_measure.fit_transform(
+                test_time_series_ls
+            )
+
         np.savez_compressed(
-            os.path.join(output_dir, "ABIDE_adjacency_" + fc_matrix_kind),
+            os.path.join(output_dir, "train_adjacency_" + fc_matrix_kind),
             a=correlation_matrices,
         )
-        correlation_matrices = np.array(correlation_matrices)
+
+        np.savez_compressed(
+            os.path.join(output_dir, "test_adjacency_" + fc_matrix_kind),
+            a=test_correlation_matrices,
+        )
 
         np.savez_compressed(os.path.join(output_dir, "Y_target"), a=y_target)
         y_target = np.array(y_target)
 
-    return correlation_matrices, y_target
+        np.savez_compressed(
+            os.path.join(output_dir, "time_series"),
+            a=time_series_ls,
+        )
+
+        np.savez_compressed(
+            os.path.join(output_dir, "test_time_series"),
+            a=test_time_series_ls,
+        )
+
+        correlation_matrices = np.array(correlation_matrices)
+        test_correlation_matrices = np.array(test_correlation_matrices)
+        time_series_ls = np.array(time_series_ls)
+        test_time_series_ls = np.array(test_time_series_ls)
+
+    return (
+        correlation_matrices,
+        test_correlation_matrices,
+        y_target,
+        time_series_ls,
+        test_time_series_ls,
+    )
 
 
 def run():
     args = parse_arguments()
-    adj_mat, y_target = load_data(
-        args.train_data_path, args.output_path, args.fc_matrix_kind
+    adj_mat, test_adj_mat, y_target, time_series, test_time_series = load_data(
+        args.train_data_path, args.test_data_path, args.output_path, args.fc_matrix_kind
     )
-    print(adj_mat.shape)
+    print("train adjacancy shape:", adj_mat.shape)
     print("***************************")
-    print(y_target.shape)
+    print("test adjacancy shape:", test_adj_mat.shape)
+    print("***************************")
+    print("y target shape:", y_target.shape)
+    print("***************************")
+    print("time series shape:", time_series.shape)
+    print("***************************")
+    print("test time series shape:", test_time_series.shape)
     print("***************************")
 
 
--- data/data_preparation.py
+++ data/data_preparation.py
@@ -14,6 +14,7 @@ from torch_geometric.data import Data
 from torch_geometric.loader import DataLoader
 from torch_geometric.data import InMemoryDataset
 from sklearn.model_selection import train_test_split
+from scipy.stats import skew, kurtosis
 
 
 def parse_arguments():
@@ -23,8 +24,15 @@ def parse_arguments():
     parser.add_argument(
         "--adj_path",
         type=str,
-        default=r"C:\Users\Afrooz Sheikholeslam\Education\8th semester\Project1\competition\out\ABIDE_adjacency.npz",
+        default=r"C:\Users\Afrooz Sheikholeslam\Education\8th semester\Project1\competition\out\train_adjacency_tangent.npz",
         help="Path to the adjacancy matrix",
+        required=True,
+    )
+    parser.add_argument(
+        "--test_adj_path",
+        type=str,
+        default=r"C:\Users\Afrooz Sheikholeslam\Education\8th semester\Project1\competition\out\test_adjacency_tangent.npz",
+        help="Path to the test adjacancy matrix",
         required=True,
     )
     parser.add_argument(
@@ -32,6 +40,20 @@ def parse_arguments():
         type=str,
         default=r"C:\Users\Afrooz Sheikholeslam\Education\8th semester\Project1\competition\out\Y_target.npz",
         help="Path to the y target",
+        required=True,
+    )
+    parser.add_argument(
+        "--time_series_path",
+        type=str,
+        default=r"C:\Users\Afrooz Sheikholeslam\Education\8th semester\Project1\competition\out\time_series.npz",
+        help="Path to the time series matrix",
+        required=True,
+    )
+    parser.add_argument(
+        "--test_time_series_path",
+        type=str,
+        default=r"C:\Users\Afrooz Sheikholeslam\Education\8th semester\Project1\competition\out\test_time_series.npz",
+        help="Path to the test time series matrix",
         required=True,
     )
     parser.add_argument(
@@ -53,7 +75,15 @@ def parse_arguments():
     return args
 
 
-def data_preparation(adj_path, y_path, batch_size=1, threshold=0.6):
+def data_preparation(
+    adj_path,
+    test_adj_path,
+    y_path,
+    time_series_path,
+    test_time_series_path,
+    batch_size=1,
+    threshold=0.6,
+):
     """Creates Data object of pytorch_geometric using graph features and edge list
 
     Parameters
@@ -69,18 +99,28 @@ def data_preparation(adj_path, y_path, batch_size=1, threshold=0.6):
     data = np.load(adj_path)
     adj_mat = data["a"]
 
+    test_data = np.load(test_adj_path)
+    test_adj_mat = test_data["a"]
+
     label = np.load(y_path)
     y_target = label["a"]
-    print(y_target.shape)
+
+    time_series_data = np.load(time_series_path)
+    time_series = time_series_data["a"]
+
+    test_time_series_data = np.load(test_time_series_path)
+    test_time_series = test_time_series_data["a"]
 
     adj_mat = np.greater_equal(adj_mat, threshold).astype(int)
+    test_adj_mat = np.greater_equal(test_adj_mat, threshold).astype(int)
 
     data_list = []
+    test_data_list = []
+
     ## Create a graph using networkx
-    for i in range(adj_mat.shape[0]):
+    for i in range(adj_mat.shape[0]):  ## same as time_series.shape[0]
         G = nx.from_numpy_matrix(adj_mat[i])
 
-        ## Extract features
         features = pd.DataFrame(
             {
                 "degree": dict(G.degree).values(),
@@ -90,13 +130,16 @@ def data_preparation(adj_path, y_path, batch_size=1, threshold=0.6):
                 "betweenness": dict(betweenness_centrality(G)).values(),
                 "closeness": dict(closeness_centrality(G)).values(),
                 "clustring_coef": dict(clustering(G)).values(),
+                "time_series_mean": time_series.mean(),
+                "time_series_variance": time_series.var(),
+                "time_series_skew": skew(time_series, axis=None),
+                "time_series_kurtosis": kurtosis(time_series, axis=None),
             }
         )
 
         X = torch.tensor(features.values)
         edge_index = torch.tensor(list(G.edges()))
 
-        # print(y_target[i].item())
         data_list.append(Data(x=X, edge_index=edge_index.T, y=y_target[i].item()))
 
     ## Split Dataset
@@ -107,16 +150,60 @@ def data_preparation(adj_path, y_path, batch_size=1, threshold=0.6):
     train_data_loader = DataLoader(train, batch_size=batch_size)
     val_data_loader = DataLoader(val, batch_size=batch_size)
 
-    return train_data_loader, val_data_loader
+    #### Create Test dataloader ######################################
+    for i in range(test_adj_mat.shape[0]):  ## same as test_time_series.shape[0]
+        G = nx.from_numpy_matrix(test_adj_mat[i])
+
+        test_features = pd.DataFrame(
+            {
+                "degree": dict(G.degree).values(),
+                "eigen_vector_centrality": dict(
+                    nx.eigenvector_centrality(G, tol=1.0e-3)
+                ).values(),
+                "betweenness": dict(betweenness_centrality(G)).values(),
+                "closeness": dict(closeness_centrality(G)).values(),
+                "clustring_coef": dict(clustering(G)).values(),
+                "time_series_mean": time_series.mean(),
+                "time_series_variance": time_series.var(),
+                "time_series_skew": skew(time_series, axis=None),
+                "time_series_kurtosis": kurtosis(time_series, axis=None),
+            }
+        )
+
+        X_test = torch.tensor(test_features.values)
+        test_edge_index = torch.tensor(list(G.edges()))
+
+        # print(y_target[i].item())
+        test_data_list.append(Data(x=X_test, edge_index=test_edge_index.T))
+
+    test_data_loader = DataLoader(test_data_list, batch_size=batch_size)
+
+    return train_data_loader, val_data_loader, test_data_loader
 
 
 def main():
     args = parse_arguments()
-    train_data_loader, val_data_loader = data_preparation(
-        args.adj_path, args.y_path, args.batch_size, args.threshold
+
+    train_data_loader, val_data_loader, test_data_loader = data_preparation(
+        args.adj_path,
+        args.test_adj_path,
+        args.y_path,
+        args.time_series_path,
+        args.test_time_series_path,
+        args.batch_size,
+        args.threshold,
     )
 
+    print("************* Train Dataloader **********************")
     for i, data in enumerate(train_data_loader):  # every batch
+        print(i, data, data.y)
+
+    print("************* Val Dataloader **********************")
+    for i, data in enumerate(val_data_loader):  # every batch
+        print(i, data, data.y)
+
+    print("************* Test Dataloader **********************")
+    for i, data in enumerate(test_data_loader):  # every batch
         print(i, data, data.y)
 
 
